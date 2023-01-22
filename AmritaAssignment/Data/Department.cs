41c50227b8a6baf409dfc9804ebd6e6d9a4eb3cc
Codingc12 <60807741+Codingc12@users.noreply.github.com>
2023-01-22 14:58:32 +0530

    Add files via upload

---
+++ AmritaAssignment/Data/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AmritaAssignment.Data
+
+
+    /*
+     
+     Created By Ramanan S
+
+     *To Store Data returned from database
+     *Each fields have getter and setter methods;
+
+
+     */
+{
+	public class Department
+	{
+        [MaxLength(10000)]
+        public int detp_id_ { get; set; }
+
+        [MaxLength(216)]
+        public string? dept_name { get; set; }
+
+        public DateTime date_ { get; set; }
+
+    }
+}
+
